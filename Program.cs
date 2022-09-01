3f5e936820df519680a38d7307817e4a503802f1
Yousef Salah <khodrojy3@gmail.com>
2022-09-01 17:53:26 +0300

    build Tags Feature

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,11 @@ builder.Services.Configure<JsonSerializerOptions>(o => o.PropertyNameCaseInsensi
 
 var app = builder.Build();
 var UserDb = new List<User>();
+var TagsCollection = new List<Tag>();
+
+TagsCollection.Add(new Tag(1, "React js"));
+TagsCollection.Add(new Tag(2, "Angular js"));
+TagsCollection.Add(new Tag(3, "Laravel"));
 
 User GetUserByToken(string Token, List<User> Users)
 {
@@ -129,6 +136,48 @@ app.MapDelete("/profiles/{PROFILENAME}/follow", (HttpContext ctx, string profile
 });
 
 #endregion
+
+#region Tags
+
+app.MapGet("/tags", async (ctx) =>
+{ 
+    await ctx.Response.WriteAsJsonAsync(new TagRequestEnv<Tag>(TagsCollection));
+});
+
+app.MapPost("/tags", async (HttpContext ctx, [FromBody] TagRequestEnv<TagRequest> req) =>
+{
+    Tag tag = new Tag(TagsCollection.Count + 1, req.Tags.First().name);
+    TagsCollection.Add(tag);
+    var list = new List<Tag>();
+    list.Add(tag);
+    await ctx.Response.WriteAsJsonAsync(new TagRequestEnv<Tag>(list)); 
+});
+
+app.MapPut("/tags", async (HttpContext ctx, [FromBody] TagRequestEnv<Tag> req) =>
+{
+    var tag = TagsCollection.FirstOrDefault(tag => tag.Id == req.Tags.First().Id);
+    Tag newTag = new Tag(tag.Id, req.Tags.First().Name);
+    TagsCollection.Remove(tag);
+    TagsCollection.Add(newTag);
+    var list = new List<Tag>();
+    list.Add(newTag);
+    await ctx.Response.WriteAsJsonAsync(new TagRequestEnv<Tag>(list));
+});
+
+app.MapDelete("/tags", (HttpContext ctx, [FromBody] TagRequestEnv<Tag> req) =>
+{
+    List<Tag> tags = new List<Tag>(req.Tags);
+
+    foreach(var tag in req.Tags)
+    {
+        TagsCollection.Remove(tag);
+    }
+
+    return ctx.Response.WriteAsJsonAsync(new TagRequestEnv<Tag>(tags));
+});
+
+
+#endregion
 app.Run();
 
 
@@ -164,6 +213,12 @@ public record UserRequest(string Username, string Email, string Password);
 public record UserRequestEnv<T>(T User);
 
 public record ProfileRequestEnv<T>(T Profile);
+
+public record TagRequestEnv<T>(List<T> Tags);
+
+public record TagRequest(string name);
+
+public record Tag(int Id, string? Name);
 
 public record User(string? UserName, string? Email, string? Password, string? Token,
     string? Bio, string? Image, List<User>? Follow);
